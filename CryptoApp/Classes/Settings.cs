41bbf8593e88734b06b17ab9f274929c6608bc02
Predrag Antanasijevic <predrag.a@elfak.rs>
2018-11-09 16:26:17 +0100

    Settings.cs and more setup.

---
+++ CryptoApp/Classes/Settings.cs
@@ -0,0 +1,62 @@
+﻿using System.IO;
+using System.Reflection;
+
+namespace CryptoApp.Classes
+{
+    internal enum Algorithm
+    {
+        DoubleTranposition,
+        XTEA,
+        CFB,
+        Knapsack,
+        MD5
+    }
+
+    internal class Settings
+    {
+
+        #region Constructors
+
+        private Settings()
+        {
+            Default();
+        }
+
+        #endregion
+
+        #region Properties
+
+        public bool FswEnabled { get; set; }
+
+        public string FswPath { get; set; }
+
+        public Algorithm Algo { get; set; }
+
+
+        #endregion
+
+        #region Methods
+
+        public void Default()
+        {
+            FswEnabled = false;
+            FswPath = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
+            Algo = Algorithm.DoubleTranposition;
+        }
+
+        #endregion
+
+        #region Singleton
+
+        private static Settings _instance;
+
+        public static Settings Instance
+        {
+            get => _instance ?? (_instance = new Settings());
+
+            set => _instance = value;
+        }
+
+        #endregion
+    }
+}
--- CryptoApp/Forms/MainForm.cs
+++ CryptoApp/Forms/MainForm.cs
@@ -7,12 +7,13 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using CryptoApp.Classes;
 
 namespace CryptoApp
 {
-    public partial class Form1 : Form
+    public partial class MainForm : Form
     {
-        public Form1()
+        public MainForm()
         {
             InitializeComponent();
         }
