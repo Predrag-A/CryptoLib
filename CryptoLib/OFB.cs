d99181aeee4310b8965a5068a2aefbf80d824671
Predrag Antanasijevic <predrag.a@elfak.rs>
2018-11-09 15:38:44 +0100

    Project Setup Completed

---
+++ CryptoLib/OFB.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CryptoLib
+{
+    internal class OFB:ICrypto
+    {
+        public bool SetKey(byte[] input)
+        {
+            throw new NotImplementedException();
+        }
+
+        public byte[] GenerateRandomKey()
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool SetIV(byte[] input)
+        {
+            throw new NotImplementedException();
+        }
+
+        public byte[] GenerateRandomIV()
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool SetAlgorithmProperties(IDictionary<string, byte[]> specArguments)
+        {
+            throw new NotImplementedException();
+        }
+
+        public byte[] Crypt(byte[] input)
+        {
+            throw new NotImplementedException();
+        }
+
+        public byte[] Decrypt(byte[] output)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
